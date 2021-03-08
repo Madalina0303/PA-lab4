5cb6fa844c5032d3dae7a42f37cca66ca1920b57
Spiridon Madalina <78969330+Madalina0303@users.noreply.github.com>
2021-03-08 20:03:37 +0200

    ..

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -10,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         var stud = IntStream.rangeClosed(0, 3).mapToObj(i -> new Student("S" + i)).toArray(Student[]::new);
 
-        List<Student> stdList = new ArrayList<>();
+        List<Student> stdList = new LinkedList<>();
         stdList.addAll(Arrays.asList(stud));
 
     }
